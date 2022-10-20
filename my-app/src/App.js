1272aad7a0b506151a173f8163c62e8c4bf78ceb
junior-slv <dirceusilva0422@gmail.com>
2022-10-20 19:32:15 -0300

    Update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className='container'>
-       <h1>Welcome, $name </h1>
+    <div className="container" > 
+      <h1>Welcome, $name</h1>
     </div>
   );
 }
